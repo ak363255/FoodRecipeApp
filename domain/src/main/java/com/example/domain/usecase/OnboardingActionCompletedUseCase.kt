e54411c303c787dc4d60b46335233bea77aca25d
Amit Kumar <ak363255@gmail.com>
2025-04-10 10:58:39 +0530

    onboarding action completed

---
+++ domain/src/main/java/com/example/domain/usecase/OnboardingActionCompletedUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.domain.usecase
+
+import androidx.datastore.preferences.core.Preferences
+import com.example.domain.model.Result
+import com.example.domain.repo.DataStoreOperationRepo
+import kotlinx.coroutines.CoroutineDispatcher
+
+class OnboardingActionCompletedUseCase(
+    private val dataStoreOperationRepo: DataStoreOperationRepo,
+    private val dispatcher : CoroutineDispatcher
+):BaseUseCase<Preferences.Key<Boolean>,Unit>(dispatcher) {
+    override suspend fun execute(parameters: Preferences.Key<Boolean>): Result<Unit> {
+    }
+
+}
